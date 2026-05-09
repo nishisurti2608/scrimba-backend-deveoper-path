cb838a2e24f366880ef79cb30969aa05fb0bbd5b
Nishi Surti <70151642+nishisurti2608@users.noreply.github.com>
2026-05-09 19:19:43 +0530

    created server

--- Node/server.js
+++ Node/server.js
@@ -1,4 +1,11 @@
+import http from 'http'
 
+const PORT = 8888;
 
+const server = http.createServer((req,res) => {
+res.end("Hello from the server")
+})
 
-console.log("Hello, Nishi Surti")
+server.listen(PORT,()=>{
+    console.log(`server is running on Port ${PORT}`)
+})
